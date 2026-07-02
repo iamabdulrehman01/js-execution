4b3016172940db111b02dfeca3cdeeca13241ecb
Abdul Rehman <abdul.rehman@coforge.com>
2026-07-02 21:44:53 +0530

    favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
-import favicon from "../assets/favicon.svg?url";
+import favicon from "../assets/newFavIcon.png";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 function NotFoundComponent() {
